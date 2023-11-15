4f90a5ffb878c9d8bbe0f590a498e4a8483aea0e
Hashem-data <150927469+Hashem-data@users.noreply.github.com>
2023-11-15 17:00:00 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+#     1.python
+#     2.Java
+#     3.R
+#     
+
+# _Objectives:_
+# 
+# . python
+# . C++
+# . R
+
+# Some of the commonly used libraries used by Data Scientists include:
+#     
+#     1.TensorFlow.
+#     2. NumPy.
+#     3. SciPy.
+#     4. Pandas.
+#     5. Matplotlib.
+#     6.Keras.
+#     7.SciKit-Learn.
+#   
+# 
+
+# |Data Science Tools|
+# |------------------|
+# |IBM SPSS|
+# |Apache Spark|
+# |Jupyter Notebook|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ##Author
+# 
+# Hashem mas
+
+# In[ ]:
+
+
+
+
